1f3f34c3c947f2973970229da97e6fe2f06d69db
Carl Yao Agbenyega <4999487+aycarl@users.noreply.github.com>
2023-08-24 15:54:21 -0400

    playing around with word pairs and classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,23 +12,40 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Mindful Moments',
+        theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue)),
+        home: const MyHomePage(title: 'Home'),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      debugShowCheckedModeBanner: false,
     );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  late MindfulMoment currentMM;
+  List listOfMM = [];
+
+  MindfulMoment makeRandomMM() {
+    var randomWordPair = WordPair.random().asString;
+    currentMM = MindfulMoment(randomWordPair);
+    listOfMM.add(currentMM);
+    return currentMM;
+  }
+}
+
+class MindfulMoment {
+  String momentName = "New MM";
+
+  late DateTime timestamp;
+
+  MindfulMoment(String name) {
+    momentName = name;
+    timestamp = DateTime.now();
   }
 }
 
